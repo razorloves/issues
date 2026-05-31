caf1f0a11a87ac6ba7c3e8e320d7cde9a4c7d6c7
LuK1337 <priv.luk@gmail.com>
2026-05-31 23:05:48 +0200

    Allow reopening issues via `/reopen`

--- label.py
+++ label.py
@@ -146,7 +146,7 @@ def main() -> None:
             '\n'.join(
                 [
                     "Hi! It appears you didn't read or follow the provided issue template.",
-                    'Please edit your issue to include the requested fields and follow the provided template, then reopen it.',
+                    'Please edit your issue to include the requested fields and follow the provided template, then reopen it by commenting `/reopen`.',
                     'For more information please see https://wiki.lineageos.org/how-to/bugreport.',
                     '',
                     'Problems:',
